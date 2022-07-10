be1d1cc9a456cb66ad1671d4e4130d6c8e853177
Derrickmaina18 <derrickmaina18@gmail.com>
2022-07-10 05:47:56 -0700

    Push task 0

--- _printf.c
+++ _printf.c
@@ -1,44 +1,37 @@
 #include "main.h"
 
 /**
- * _printf - produces output according to a format.
- * @format: character string
- * Return: charsprinted
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formated string
+ * @format: A string containing all the desired characters
+ * Return: A total count of the characters printed
  */
-
 int _printf(const char *format, ...)
 {
-	int charsprinted = 0, int i = 0;
-	va_list argumentsprintf;
-	char ;
+	int printed_chars;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
+		{NULL, NULL}
+	};
+	va_list arg_list;
 
-	va_start(argumentsprintf, format);
+	if (format == NULL)
+		return (-1);
 
-	for (; format; i++)
-	{
-		if (format[i] == '%')
-		{
-			if (format == NULL)
-				return(-1);
-			else if(format[i + 1] == '\o')
-			{
-				return(-1);
-			}
-			else if (format[i] == 'c')
-			{
-				_putchar("c\n");
-			}
-			else if (format[i] == 's')
-			{
-				_putchar("s\n");
-			}
-		_putchar("%\n");
-		return (0);
-		}
-		i++;
-		else
-		return (NULL);
-	}
-	va_end(argumentsprintf);
-	return (charsprinted);
+	va_start(arg_list, format);
+	/*Calling parser function*/
+	printed_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
 }
