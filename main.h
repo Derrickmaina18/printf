d65514c2b5f338f4cecaad1442faad994b8cb277
Mulubi <Mulubiwarren@gmail.com>
2022-07-12 02:08:09 -0700

    Debugged the main header file

--- main.h
+++ main.h
@@ -1,6 +1,29 @@
-#ifndef PRINTF
-#define PRINTF
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * struct print - this is a function showing the structure for
+ * printer printer functions
+ *
+ * @type_arg: used as an identifier variable
+ * @f: pointer to a printer function
+ *
+ * Description: program contains struct that stores pointers to a printer functions.
+ */
+
+typedef struct print
+{
+	char *type_arg;
+	int (*f)(va_list);
+} print_t;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_c(va_list c);
+int print_s(va_list s);
+
 #endif
