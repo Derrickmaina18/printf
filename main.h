d08d5a989fa606182cf2e74493c2e4ef8d46ba13
Mulubi <Mulubiwarren@gmail.com>
2022-07-12 05:09:42 -0700

    Fixed the error in main file

--- main.h
+++ main.h
@@ -25,6 +25,7 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int print_c(va_list c);
 int print_s(va_list s);
-int (*helper_function_printf(const char *format))(va_list)
+int (*helper_function_printf(const char *format))(va_list);
+int return_id_helper(const char *format);
 
 #endif
