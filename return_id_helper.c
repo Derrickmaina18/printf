5c5ae57ca4121147756690f27a9acf43a7af66b8
Mulubi <Mulubiwarren@gmail.com>
2022-07-12 05:35:21 -0700

    Corrected some errors

--- return_id_helper.c
+++ return_id_helper.c
@@ -8,26 +8,31 @@
  * Return: amount of identifiers
  */
 
-int return_id_helper(const char *format)
+int return_id_helper(const char *format, int index)
 {
 	print_t f_list[] = {
 		{"c", print_c},
 		{"s", print_s},
 		{NULL, NULL},
 	};
-	int i = 0, j = 0;
+	int i = 0, j = 0, first_index;
 
+	first_index = index;
 	while (f_list[i].type_arg)
 	{
-		if (f_list[i].type_arg[j + i] '\0')
-			j++;
+		if (format[index] == f_list[i].type.arg[j])
+		{
+			if (f_list[i].type_arg[j + i] != '\0')
+				index++, j++;
+			else
+				break;
+		}
 		else
-			break;
-	}
-	else
-	{
-		j = 0;
-		i++;
+		{
+			j = 0;
+			i++;
+			index = first_index;
+		}
 	}
 	return (j);
 }
