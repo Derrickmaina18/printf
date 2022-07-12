d15a0aac1d6cb90c7aaff96a26788d84819d0982
Mulubi <Mulubiwarren@gmail.com>
2022-07-12 05:10:48 -0700

    Debbugged printf file

--- _printf.c
+++ _printf.c
@@ -15,36 +15,24 @@ int _printf(const char *format, ...)
 	va_list argumentsprintf;
 	int (*f)(va_list);
 
-	if (format == NULL)
-		return (-1);
-
 	va_start(argumentsprintf, format);
 
-	while (format[i])
-	{
-		for (; format[i] != '%' && format[i]; i++)
-		{
-			_putchar(format[i]);
-			charsprinted++;
-		}
-		if (!format[i])
-			return (charsprinted);
+	if (!format || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return(0);
 
-		f = helper_function_printf(&format[i + 1]);
+		for (i = 0; format && format[i]; i++)
+		{
+		if (!format[i] == '%')
+		f = helper_function_printf(format, i + 1);
+
 		if (f != NULL)
 		{
 			charsprinted += f(argumentsprintf);
-			i += 2;
-			continue;
+			i += return_id_helper(format, i + 1);
 		}
-		if (!format[i + 1])
-			return (-1);
-		_putchar(format[i]);
-		charsprinted++;
-		if (format[i + 1] == '%')
-			i += 2;
 		else
-			i++;
 	}
 	va_end(argumentsprintf);
 	return (charsprinted);
