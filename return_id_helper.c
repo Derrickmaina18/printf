3d9d01a87f5e62b668fea638f1ce4401b262370e
Mulubi <Mulubiwarren@gmail.com>
2022-07-12 04:59:23 -0700

    Added a helping function that returns the amount of identifiers

---
+++ return_id_helper.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * return_id_helper - function that helps to return the amount of identifiers
+ *
+ * @format: argument identifier
+ *
+ * Return: amount of identifiers
+ */
+
+int return_id_helper(const char *format)
+{
+	print_t f_list[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{NULL, NULL},
+	};
+	int i = 0, j = 0;
+
+	while (f_list[i].type_arg)
+	{
+		if (f_list[i].type_arg[j + i] '\0')
+			j++;
+		else
+			break;
+	}
+	else
+	{
+		j = 0;
+		i++;
+	}
+	return (j);
+}
